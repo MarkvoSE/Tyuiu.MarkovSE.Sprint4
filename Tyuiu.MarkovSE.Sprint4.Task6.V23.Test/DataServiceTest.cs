d00a07c8cdb844cc7f37ba0a03d7f22e14403396
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 07:33:55 +0500

    Task6

---
+++ Tyuiu.MarkovSE.Sprint4.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MarkovSE.Sprint4.Task6.V23.Lib;
+namespace Tyuiu.MarkovSE.Sprint4.Task6.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService dataService = new DataService();
+            string[] company = new string[] { "Microsoft", "Google", "Apple", "Amazon", "Facebook", "Tesla", "Netflix" };
+            Assert.AreEqual(2, dataService.Calculate(company));
+
+        }
+    }
+}
