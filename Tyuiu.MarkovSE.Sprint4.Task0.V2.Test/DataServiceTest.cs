a8ced9c8fefb8b84e309b8d24f399ae32e5910a8
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-26 06:41:40 +0500

    Task0

---
+++ Tyuiu.MarkovSE.Sprint4.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MarkovSE.Sprint4.Task0.V2.Lib;
+namespace Tyuiu.MarkovSE.Sprint4.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int wait = 44;
+            int[] array = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetSumOddArrEl(array);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
