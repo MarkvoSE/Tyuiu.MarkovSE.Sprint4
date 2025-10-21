a4558da71473502e124b1d11ee99921bd06451c7
MarkvoSE <markovse750@gmail.com>
2025-10-21 22:00:47 +0500

    Add files via upload

---
+++ Tyuiu.MarkovSE.Sprint4.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MarkovSE.Sprint4.Task2.V4.Lib;
+namespace Tyuiu.MarkovSE.Sprint4.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 0, 3, 2, 7, 8, 9, 2, 2, 2, 2, 2, 2 };
+            int res = ds.Calculate(array);
+            int wait = 19;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
